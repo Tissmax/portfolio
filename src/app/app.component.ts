f751664f5dbb88ebed0233202f04d769337c72d0
Mathis <mathis.basier@gmail.com>
2024-04-01 23:04:23 +0200

    fond du site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, 
+            RouterOutlet,
+            AboutComponent,
+            ContactComponent,
+            HomeComponent,
+            ProjectComponent,
+            NavComponent,
+          ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'mathis-basier';
+  title = 'Mathis-Basier';
 }
